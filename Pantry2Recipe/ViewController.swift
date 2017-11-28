28cf33512f6f84138d9877953d6d7dac989e46c7
Zach Wilner <Zach.wilner@gmail.com>
2017-11-28 17:21:31 -0500

    Initial Commit

---
+++ Pantry2Recipe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pantry2Recipe
+//
+//  Created by Zach Wilner on 11/28/17.
+//  Copyright © 2017 Zach Wilner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
